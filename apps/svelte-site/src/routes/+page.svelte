f716c56a31e27b264867d46faa6dd9c58e0cad3f
Alex <alex@codingcat.dev>
2023-04-16 18:16:30 -0400

    inputs-bindings-and-reactivity

--- apps/svelte-site/src/routes/+page.svelte
+++ apps/svelte-site/src/routes/+page.svelte
@@ -1,13 +1,5 @@
 <script>
-	import Kitten from './Kitten.svelte';
+	import CatAge from './CatAge.svelte';
 </script>
 
-<h1>Hello Kittens</h1>
-
-<p>This wild and crazy cats!</p>
-
-<Kitten />
-<Kitten
-	src={'https://media.tenor.com/fTTVgygGDh8AAAAC/kitty-cat-sandwich.gif'}
-	name={'sandwhich cat'}
-/>
+<CatAge />
---
+++ apps/svelte-site/src/routes/CatAge.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	let age = 0;
+	let ages: { age: number; calculatedAge: number }[] = [];
+
+	$: ages = [...ages, { age, calculatedAge }];
+
+	$: calculatedAge = age < 1 ? 0 : age * 6;
+	$: if (age < 0) {
+		alert(`your cat isn't born yet?`);
+	}
+</script>
+
+<input type="number" bind:value={age} />
+<p>{calculatedAge}</p>
+
+<ul>
+	{#each ages as a}
+		<li>{`${a.age}-${a.calculatedAge}`}</li>
+	{/each}
+</ul>
